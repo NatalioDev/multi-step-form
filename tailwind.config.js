c6fc40b799d4588e152588777d6e2d108861202b
NatalioDev <nataliodg.dev@gmail.com>
2023-11-17 17:49:20 -0300

    add project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+export default  {
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      height: {
+        'md-height': '70%',
+      },
+      width: {
+        'lg-width': '70%',
+        'md-sidebar-width': '45%',
+        'lg-sidebar-width': '35%'
+      }
+    },
   },
   plugins: [],
 }
-
